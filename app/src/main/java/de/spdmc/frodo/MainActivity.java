6504d5903e97e4796d7ef4bb60b47254ff4e4bdc
bertosch <robertbraune87@gmail.com>
2016-01-25 10:52:52 +0100

    floating Action Button aus MainActivity entfernt!

--- app/src/main/java/de/spdmc/frodo/MainActivity.java
+++ app/src/main/java/de/spdmc/frodo/MainActivity.java
@@ -2,8 +2,6 @@ package de.spdmc.frodo;
 
 import android.database.DataSetObserver;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
@@ -31,14 +29,14 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
         buttonSend = (Button) findViewById(R.id.send);
         listView = (ListView) findViewById(R.id.msgoverview);
         chatArrayAdapter = new ChatArrayAdapter(getApplicationContext(), R.layout.right);
