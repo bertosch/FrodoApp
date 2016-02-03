198913b49f136cad5f055ea7a5d80319bfe29eba
lars <lars-kolmetz@gmx.de>
2016-02-03 23:20:16 +0100

    RecommendationParser zum Parsen von Empfehlungsanforderung durch den User geaendert. (User muss empfiehl* oder empfehl* sagen)

--- app/src/main/java/de/spdmc/frodo/textparser/RecommendationParser.java
+++ app/src/main/java/de/spdmc/frodo/textparser/RecommendationParser.java
@@ -2,16 +2,13 @@ package de.spdmc.frodo.textparser;
 
 import de.spdmc.frodo.enumerations.Enumerations;
 
-/**
- * Created by lars on 28.01.16.
- */
 public class RecommendationParser extends Parser {
 
     @Override
     public InputContent parse(String in) {
         in = normalize(in);
         InputContent ic = new InputContent();
-        if(in.equals("gib mir eine empfehlung")){
+        if(in.contains("empfiehl") || in.contains("empfehl")){
             ic.setDialogState(Enumerations.DialogState.RECOMMEND);
         }
         return ic;
