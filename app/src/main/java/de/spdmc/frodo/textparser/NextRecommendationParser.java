4b408e1600a2fa65a371f728f767ffae42c792d1
lars <lars-kolmetz@gmx.de>
2016-02-10 14:24:45 +0100

    NextRecommendationParser verbessert (Synonyme)

--- app/src/main/java/de/spdmc/frodo/textparser/NextRecommendationParser.java
+++ app/src/main/java/de/spdmc/frodo/textparser/NextRecommendationParser.java
@@ -7,9 +7,12 @@ public class NextRecommendationParser extends Parser {
     @Override
     public InputContent parse(String in) {
         InputContent ic = new InputContent();
-        if(((in.contains("schon") || in.contains("bereits")) && (in.contains("seh") || in.contains("kenn")))
+        if(in.contains("nein") || in.contains("kenn")
+                || (in.contains("schon") || in.contains("bereits")) && (in.contains("seh"))
                 || ((in.contains("mag") || in.contains("zufrieden")
-                || in.contains("glücklich") || in.contains("gluecklich")) && in.contains("nicht"))){
+                || in.contains("glücklich") || in.contains("gluecklich") || in.contains("geschmack")
+                || in.contains("gut") || in.contains("gefällt") || in.contains("gefaellt"))
+                && in.contains("nicht"))){
             ic.setDialogState(Enumerations.DialogState.NEXT_RECOMMENDATION);
         } else ic.setDialogState(Enumerations.DialogState.NEXT_RECOMMENDATION_FAULT);
         return ic;
