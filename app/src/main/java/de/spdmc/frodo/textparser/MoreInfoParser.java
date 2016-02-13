cd64324e98c696011d33b23a7cc52d68a246c4a6
lars <lars-kolmetz@gmx.de>
2016-02-13 16:51:38 +0100

    Erkennung erweitert

--- app/src/main/java/de/spdmc/frodo/textparser/MoreInfoParser.java
+++ app/src/main/java/de/spdmc/frodo/textparser/MoreInfoParser.java
@@ -7,7 +7,8 @@ public class MoreInfoParser extends Parser {
     @Override
     public InputContent parse(String in) {
         InputContent ic = new InputContent();
-        if(in.contains("mehr") || in.contains("weitere") || in.contains("inhalt") || in.contains("beschreib") || in.startsWith("was")){
+        if(in.contains("mehr") || in.contains("weitere") || in.contains("inhalt") || in.contains("beschreib") || in.contains("zusammenfass")
+                || in.startsWith("was") || in.contains("worum")){
             ic.setDialogState(Enumerations.DialogState.MORE_INFO);
         }
         return ic;
