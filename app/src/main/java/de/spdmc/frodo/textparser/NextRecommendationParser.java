04df59084eec32da8d8f913f2deed65ce803909c
lars <lars-kolmetz@gmx.de>
2016-02-13 16:52:33 +0100

    Input normalisiert, Erkennung erweitert

--- app/src/main/java/de/spdmc/frodo/textparser/NextRecommendationParser.java
+++ app/src/main/java/de/spdmc/frodo/textparser/NextRecommendationParser.java
@@ -6,15 +6,18 @@ public class NextRecommendationParser extends Parser {
 
     @Override
     public InputContent parse(String in) {
+        in = normalize(in);
+        String[] inArr = in.split(" ");
         InputContent ic = new InputContent();
-        if(in.contains("nein") || in.contains("kenn")
+        if(inArr[0].equals("nein") || inArr[0].equals("nö") || inArr[0].equals("nä") || inArr[0].equals("ne")
+                || inArr[0].startsWith("nee") || inArr[0].contains("nöö") || inArr[0].contains("nää") || in.contains("kenn")
                 || (in.contains("schon") || in.contains("bereits")) && (in.contains("seh"))
                 || ((in.contains("mag") || in.contains("zufrieden")
                 || in.contains("glücklich") || in.contains("gluecklich") || in.contains("geschmack")
                 || in.contains("gut") || in.contains("gefällt") || in.contains("gefaellt"))
                 && in.contains("nicht"))){
             ic.setDialogState(Enumerations.DialogState.NEXT_RECOMMENDATION);
-        } else ic.setDialogState(Enumerations.DialogState.NEXT_RECOMMENDATION_FAULT);
+        } //else ic.setDialogState(Enumerations.DialogState.NEXT_RECOMMENDATION_FAULT);
         return ic;
     }
 
